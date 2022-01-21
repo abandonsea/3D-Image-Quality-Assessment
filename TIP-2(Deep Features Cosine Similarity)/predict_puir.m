ce75e0931c42b0b9f630d64a64624833a315e10a
sadbhawnathakur <43072375+sadbhawnathakur@users.noreply.github.com>
2022-01-21 12:51:32 +0530

    convrted to .p

--- TIP-2(Deep Features Cosine Similarity)/predict_puir.m
+++ TIP-2(Deep Features Cosine Similarity)/predict_puir.m
@@ -1,35 +1,9 @@
-inp1 = double((imread(['syn.png'])));
-inp2 = double((imread(['org.png'])));
-inp1=rgb2ycbcr(inp1);
-inp2=rgb2ycbcr(inp2);
- [m,n]=size(inp1);
- v=abs(inp1-inp2);
- tip=v;
-se = strel('square',5);
-v = imerode(v,se);
-
-se = strel('square',5);
-v=imdilate(v,se);
-blur=log(255^2*(mean2(v.^2)/mean2(inp2)));
-inp3=inp1;
-inp4=inp2;
-count = 0;
-for i=1:m
-    for j=1:n/3
-        if v(i,j,1) >25 || v(i,j,2) >25 || v(i,j,3) >25
-            count=count+1;
-        end
-    end
-end
+inp1 = double((imread(['1.png'])));
+inp2 = double((imread(['141.png'])));
 
 % IETR dataset parameters
-ii = 5.4100;
-iii = 3.1100;
-tt = 600;
-if count> abs((m*n)/tt)
-    score=blur*ii;
- else  
-    score=blur*iii;
-end
+lam_1 = 5.4100;
+lam_2 = 3.1100;
+gam = 600;
 
- score
+ score = puir(inp1, inp2, lam_1, lam_2, gam)
